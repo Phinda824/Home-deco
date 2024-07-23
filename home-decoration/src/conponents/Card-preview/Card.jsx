39d286573f9f2882848610ac42b444e1184127ce
Phinda Simanga <simanga824@gmail.com>
2024-07-23 23:09:22 +0200

    updates

--- home-decoration/src/conponents/Card-preview/Card.jsx
+++ home-decoration/src/conponents/Card-preview/Card.jsx
@@ -1,36 +1,7 @@
 import React from "react";
-import chair8 from "/images/chair8.webp";
-import chair7 from "/images/chair7.avif";
-import chair9 from "/images/chair9.jpg";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
-import "swiper/swiper-bundle.min.css";
 
 const Card = () => {
-  return (
-    <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={2}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-    >
-      <SwiperSlide>
-        <img src={chair7} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={chair7} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={chair7} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={chair7} alt="" />
-      </SwiperSlide>
-    </Swiper>
-  );
+  return <div>card</div>;
 };
 
 export default Card;
--- home-decoration/src/conponents/Content/Content.jsx
+++ home-decoration/src/conponents/Content/Content.jsx
@@ -1,35 +1,95 @@
 import React from "react";
 import "./Content.css";
+import { motion } from "framer-motion";
+
 import chair8 from "/images/chair8.webp";
-import chair7 from "/images/chair7.avif";
-import chair9 from "/images/chair9.jpg";
+import couch2 from "/images/couch2.webp";
+import table1 from "/images/Table1.jpg";
+import tvStand1 from "/images/Tv-stand1.webp";
 
 const Content = () => {
   return (
     <div className="content-section">
-      <h1>See Our Quality Product</h1>
+      <motion.h1
+        initial={{ y: 50, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{
+          delay: 0.4,
+          y: { type: "spring", stiffness: 60 },
+          opacity: { duration: 0.2 },
+          ease: "easeIn",
+          duration: 1,
+        }}
+      >
+        See Our Quality Product
+      </motion.h1>
       <div className="content-sect">
-        <div className="card-sect">
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{
+            delay: 0.2,
+            y: { type: "spring", stiffness: 60 },
+            opacity: { duration: 0.2 },
+            ease: "easeIn",
+            duration: 1,
+          }}
+          className="card-sect"
+        >
           <div className="image">
             <img src={chair8} alt="" />
           </div>
-          <p>Leather Cover Sofa</p>
-          <div className="price">R800.00</div>
+          <p>Comfortable Chair</p>
+          <div className="rating">
+            <i class="fa-solid fa-star"></i>
+            <div className="rate">4.5</div>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{
+            delay: 1,
+            y: { type: "spring", stiffness: 60 },
+            opacity: { duration: 0.5 },
+            ease: "easeIn",
+            duration: 1,
+          }}
+          className="card-sect"
+        >
+          <div className="image">
+            <img src={tvStand1} alt="" />
+          </div>
+          <p>Tv Stand</p>
+          <div className="rating">
+            <i class="fa-solid fa-star"></i>
+            <div className="rate">4.5</div>
+          </div>
+        </motion.div>
+        <div className="card-sect">
+          <div className="image">
+            <img src={couch2} alt="" />
+          </div>
+          <p>Comfortable Couch</p>
+          <div className="rating">
+            <i class="fa-solid fa-star"></i>
+            <div className="rate">4.5</div>
+          </div>
         </div>
         <div className="card-sect">
           <div className="image">
-            <img src={chair7} alt="" />
+            <img src={table1} alt="" />
           </div>
-          <p>Leather Cover Sofa</p>
-          <div className="price">R800.00</div>
-        </div>
-        <div className="card-sect">
-          <div className="image">
-            <img src={chair9} alt="" />
+          <p>Coffee Table</p>
+          <div className="rating">
+            <i class="fa-solid fa-star"></i>
+            <div className="rate">4.5</div>
           </div>
-          <p>Leather Cover Sofa</p>
-          <div className="price">R800.00</div>
         </div>
+      </div>
+
+      <div className="product-btn">
+        <button className="btn">Vie All Products</button>
       </div>
     </div>
   );
